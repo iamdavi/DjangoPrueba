52bf7154ac9ef16e2aa2253db4add5ad55ce215f
iamdavi <itsdavid.otero@gmail.com>
2018-12-26 18:06:24 +0100

    First commit

---
+++ catalog/admin.py
@@ -0,0 +1,73 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Author, Genre, Book, BookInstance
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+
+# Define the admin class
+#class AuthorAdmin(admin.ModelAdmin):
+#    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+#    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+#admin.site.register(Author, AuthorAdmin)
+
+
+class BookInLine(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInLine]
+
+
+# Register the Admin classes for Book using the decorator
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id', 'get_isbn', 'get_author')
+    fieldsets = (
+        ('Book information', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+    def get_isbn(self, obj):
+        """
+        String para el isbn del libro
+        """
+        return obj.book.isbn
+        
+    get_isbn.short_description = 'ISBN'
+
+    def get_author(self, obj):
+        """
+        String para obtener el autor del libro
+        """
+
+        return obj.book.author
+
+    get_author.short_description = 'Autor'
+
